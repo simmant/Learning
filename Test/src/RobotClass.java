9e267574aa3a330203f40b1bb5f8dbcd8cfb75b2
yadavs192 <yadavs192@WUK5024016-D.worldpay.local>
2018-08-20 19:20:44 +0530

    TEST

--- Test/src/RobotClass.java
+++ Test/src/RobotClass.java
@@ -4,12 +4,16 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
 
+/**
+ * @author yadavs192
+ *
+ */
 public class RobotClass {
 	static Robot robot;
 
 	public static void main(String... strings) throws Exception {
 		robot = new Robot();
-		Desktop.getDesktop().open(new File("path/to/google/chrome"));
+		Desktop.getDesktop().open(new File("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"));
 		robot.delay(2000);
 		robot.mouseMove(148, 50);
 		robot.delay(100);
@@ -21,8 +25,10 @@ public class RobotClass {
 	}
 
 	public static void keyPress() {
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
+		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
 		robot.delay(100);
+		// robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
+		// robot.delay(100);
 		robot.keyPress(KeyEvent.VK_S);
 		robot.delay(100);
 		robot.keyPress(KeyEvent.VK_I);
@@ -40,6 +46,12 @@ public class RobotClass {
 		robot.keyPress(KeyEvent.VK_ENTER);
 		robot.delay(5000);
 		robot.mouseMove(176, 473);
+		robot.delay(100);
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.delay(200);
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+		robot.delay(7000);
+		robot.mouseMove(1898, 15);
 		robot.delay(100);
 		robot.mousePress(InputEvent.BUTTON1_MASK);
 		robot.delay(200);
